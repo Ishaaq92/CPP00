9d1cd5cb87b16dcdae522b61ce53db101993e8a2
ishaaq <a.ishaaq@icloud.com>
2025-08-28 11:05:04 +0100

    Adjusted the makefile. No there will be no re-linking.

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,8 +6,8 @@ int	main(int ac, char **av)
 	int	j;
 
 	i = 0;
-	if (ac == 1)
-		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *", 0);
+	if (ac != 2)
+		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 0);
 	while (av[++i] != NULL)
 	{
 		j = -1;
@@ -19,5 +19,6 @@ int	main(int ac, char **av)
 				std::cout << av[i][j];
 		}
 	}
+	std::cout << std::endl;
 	return (0);
 }
